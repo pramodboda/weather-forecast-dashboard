8a93b88eb3153e62bf382b8f8eef39b55b8cb519
pramodboda <pramodkumarboda@gmail.com>
2024-03-13 02:59:34 +0530

    UPDATE: Setting up SVG Icons

--- src/assets/weather-icons/HumiditySVGIcon.jsx
+++ src/assets/weather-icons/HumiditySVGIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const HumiditySVGIcon = (props) => (    <svg
+const HumiditySVGIcon = (props) => (<svg
   xmlns="http://www.w3.org/2000/svg"
   xmlnsXlink="http://www.w3.org/1999/xlink"
   viewBox="0 0 512 512"
